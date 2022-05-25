f880d86301400b3fc272fff4d03a502f5d6b9775
qwerty <106223006+qwerty-lol@users.noreply.github.com>
2022-05-24 21:46:18 -0700

    Add files via upload

---
+++ redeem every code script.lua
@@ -0,0 +1,95 @@
+local args = {
+    [1] = "BLOXY"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(1)
+local args = {
+    [1] = "pog"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(1)
+local args = {
+    [1] = "ROLVE"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(1)
+local args = {
+    [1] = "KITTEN"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(1)
+local args = {
+    [1] = "JOHN"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(1)
+local args = {
+    [1] = "CBROX"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(1)
+local args = {
+    [1] = "EPRIKA"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(1)
+local args = {
+    [1] = "FLAMINGO"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(1)
+local args = {
+    [1] = "POKE"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(1)
+local args = {
+    [1] = "PET"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(1)
+local args = {
+    [1] = "ANNA"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(1)
+local args = {
+    [1] = "Bandites"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(1)
+local args = {
+    [1] = "F00LISH"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(1)
+local args = {
+    [1] = "GARCELLO"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(1)
+local args = {
+    [1] = "xonae"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
+wait(1)
+local args = {
+    [1] = "E"
+}
+
+game:GetService("ReplicatedStorage").Redeem:InvokeServer(unpack(args))
